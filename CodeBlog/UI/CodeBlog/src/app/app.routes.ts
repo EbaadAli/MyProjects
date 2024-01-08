d417301d8f18d1634555bb4ddd2330c766b70b24
unknown <ebaadali@gmail.com>
2024-01-08 13:01:04 -0500

    fixed routing issues - category list is now accessable via routing

--- CodeBlog/UI/CodeBlog/src/app/app.routes.ts
+++ CodeBlog/UI/CodeBlog/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './features/Category/category-list/category-list.component';
+
+export const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
